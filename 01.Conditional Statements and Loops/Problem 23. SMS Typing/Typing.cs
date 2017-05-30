f470c50356d6e96b196f77a1cf8a5aede99ae5c4
Aleks Mitev <ajmitev@gmail.com>
2017-05-30 23:23:29 +0300

    Update after first lecture.

---
+++ 01.Conditional Statements and Loops/Problem 23. SMS Typing/Typing.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Typing
+{
+    public static void Main()
+    {
+        //Input
+        int characterCount = int.Parse(Console.ReadLine());
+
+        //Logic
+
+        string[][] smsTable = new string[][]
+             {
+                new string[]{" "},
+                new string[]{ },
+                new string[]{"a", "b","c"},
+                new string[]{"d","e","f"},
+                new string[]{"g","h","i"},
+                new string[]{"j","k","l" },
+                new string[]{"m","n","o"},
+                new string[]{"p","q","r","s"},
+                new string[]{"t","u","v" },
+                new string[]{"w","x","y","z" }
+             };
+
+        string outputSms = "";
+        for (int i = 0; i < characterCount; i++)
+        {
+            int charCode = int.Parse(Console.ReadLine());
+            outputSms += smsTable[charCode % 10][charCode.ToString().Length - 1];
+        }
+        Console.WriteLine(outputSms);
+
+    }
+}
