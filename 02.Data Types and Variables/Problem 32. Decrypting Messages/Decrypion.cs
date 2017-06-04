a28c0e7da36c6f4c2eabca018d26f948845940c1
AJMitev <ajmitev@gmail.com>
2017-06-04 21:48:54 +0300

    Update Problems 32, 33 & 34

---
+++ 02.Data Types and Variables/Problem 32. Decrypting Messages/Decrypion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Decrypion
+{
+    public static void Main()
+    {
+        //Input
+        int key = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+
+        //Logic
+        string decrypted = string.Empty;
+
+        for (int i = 0; i < n; i++)
+        {
+            char currChar = char.Parse(Console.ReadLine());
+            int newChar = currChar + key;
+            decrypted += (char)newChar;
+        }
+
+        //Logic
+        Console.WriteLine(decrypted);
+    }
+}
---
+++ 02.Data Types and Variables/Problem 33. Boat Simulator/Race.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public class Race
+{
+    public static void Main()
+    {
+        //Input
+        char firstPlayer = char.Parse(Console.ReadLine());
+        char secondPlayer = char.Parse(Console.ReadLine());
+        int moves = int.Parse(Console.ReadLine());
+
+        //Logic
+        int firstBoatScore = 0;
+        int secondBoatScore = 0;
+        bool isItBreaked = false;
+
+        for (int i = 1; i <= moves; i++)
+        {
+            string currentMove = Console.ReadLine();
+            int currentScore = currentMove.Length;
+
+            if (firstBoatScore >= 50)
+            {
+                isItBreaked = true;
+                Console.WriteLine(firstPlayer);
+                break;
+
+            }
+            else if (secondBoatScore >= 50)
+            {
+                isItBreaked = true;
+                Console.WriteLine(secondPlayer);
+                break;
+            }
+
+            if (currentMove.Equals("UPGRADE"))
+            {
+                int temp = firstPlayer + 3;
+                firstPlayer = (char)temp;
+                temp = secondPlayer + 3;
+                secondPlayer = (char)temp;
+            }
+            else
+            {
+                if (i % 2 == 1)
+                {
+                    firstBoatScore += currentScore;
+                }
+                else
+                {
+                    secondBoatScore += currentScore;
+                }
+
+            }
+        }
+
+        //Output if its not breaked
+        if (!isItBreaked)
+        {
+            if (firstBoatScore > secondBoatScore)
+            {
+                Console.WriteLine(firstPlayer);
+            }
+            else
+            {
+                Console.WriteLine(secondPlayer);
+            }
+        }
+
+    }
+}
