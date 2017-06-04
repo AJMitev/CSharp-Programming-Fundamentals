a2cb490b4c1549fd87dd8c8d0e89aa4d416db526
AJ <ajmitev@gmail.com>
2017-06-04 14:30:44 +0300

    Update Problem 20, 21 & 22

---
+++ 02.Data Types and Variables/Problem 20. Type Boundaries/Boundaries.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Boundaries
+{
+    public static void Main()
+    {
+        //Input
+        string type = Console.ReadLine();
+
+        //Logic
+        switch (type)
+        {
+            case "int": Console.WriteLine(int.MaxValue); Console.WriteLine(int.MinValue); break;
+            case "uint": Console.WriteLine(uint.MaxValue); Console.WriteLine(uint.MinValue); break;
+            case "long": Console.WriteLine(long.MaxValue); Console.WriteLine(long.MinValue); break;
+            case "byte": Console.WriteLine(byte.MaxValue); Console.WriteLine(byte.MinValue); break;
+            case "sbyte": Console.WriteLine(sbyte.MaxValue); Console.WriteLine(sbyte.MinValue); break;
+            default: Console.WriteLine("Wrong input! Please try again.");  break;
+        }
+
+    }
+}
---
+++ 02.Data Types and Variables/Problem 21. Number Checker/NumberCheker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class NumberCheker
+{
+    public static void Main()
+    {
+        //Input
+        string input = Console.ReadLine();
+
+        //Logic
+        try
+        {
+            long number = long.Parse(input);
+            Console.WriteLine("integer");
+        }
+        catch (Exception)
+        {
+
+            Console.WriteLine("floating-point");
+        }
+
+
+    }
+}
