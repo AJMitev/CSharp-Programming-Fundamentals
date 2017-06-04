32eff23844669d578c67e6133d1dafe6951435e7
AJ <ajmitev@gmail.com>
2017-06-04 18:21:05 +0300

    Update Problem 31

---
+++ 02.Data Types and Variables/Problem 31. Beer Kegs/BiggerKeg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class BiggerKeg
+{
+    public static void Main()
+    {
+        //Input
+        int numberOfLines = int.Parse(Console.ReadLine());
+
+        string biggestKeg = string.Empty;
+        double biggestVolume = 0.0;
+
+        for (int i = 0; i < numberOfLines ; i++)
+        {
+            string kegModel = Console.ReadLine();
+            double radius = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double currentVolume = Math.PI * Math.Pow(radius,2) * height;
+
+            if (currentVolume > biggestVolume)
+            {
+                biggestKeg = kegModel;
+                biggestVolume = currentVolume;
+            }
+           
+        }
+
+        //Output
+        Console.WriteLine(biggestKeg);
+    }
+}
