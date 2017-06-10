5e396f34ab97b5884f4851c46627c0b3b528e32a
ajmitev <ajmitev@gmail.com>
2017-06-10 13:17:03 +0300

    Update Problems 2 & 3

---
+++ 04.Arrays/Problem 02. Rotate and Sum/SumTwoArrays.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+public class SumTwoArrays
+{
+    public static void Main()
+    {
+        //Input
+        long[] numbers = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+        long rotator = long.Parse(Console.ReadLine());
+
+        //Logic
+        long[] sumArr = new long[numbers.Length];
+
+        for (long r = 0; r < rotator; r++)
+        {
+            RotateArray(numbers);
+
+            for (long i = 0; i < numbers.Length; i++)
+            {
+                sumArr[i] += numbers[i];
+            }
+        }
+
+        //Output
+        Console.WriteLine(string.Join(" ", sumArr));
+
+    }
+
+    private static void RotateArray(long[] numbers)
+    {
+        long lastElement = numbers[numbers.Length-1];
+
+        for (int i = numbers.Length-1; i > 0 ; i--)
+        {
+            numbers[i] = numbers[i - 1];
+        }
+        numbers[0] = lastElement;
+    }
+}
---
+++ 04.Arrays/Problem 03. Fold and Sum/DropAndSumArray.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+public class DropAndSumArray
+{
+    public static void Main()
+    {
+        ///Input
+        int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        ///Logic
+        //Extract Leftside numbers
+        int[] leftDropedNumbers = new int[numbers.Length / 4];
+
+        for (int i = 0; i < leftDropedNumbers.Length; i++)
+        {
+            leftDropedNumbers[i] = numbers[i];
+        }
+
+        //Extract Middle
+        int[] reservedNumbers = new int[numbers.Length / 2];
+
+        for (int middle = 0; middle < reservedNumbers.Length; middle++)
+        {
+            reservedNumbers[middle] = numbers[numbers.Length -reservedNumbers.Length- leftDropedNumbers.Length+middle];
+        }
+
+        //Extract Rightside numbers
+        int[] rightDropedNumbers = new int[numbers.Length / 4];
+
+        for (int rightIndex = 0; rightIndex < rightDropedNumbers.Length; rightIndex++)
+        {
+            rightDropedNumbers[rightIndex] = numbers[numbers.Length - rightDropedNumbers.Length + rightIndex];
+        }
+
+        int[] foldedNumbers = new int[reservedNumbers.Length];
+
+        for (int i = leftDropedNumbers.Length -1, counter =0 ; i >= 0; i--)
+        {
+            foldedNumbers[counter] = leftDropedNumbers[i];
+            counter++;
+        }
+
+        for (int i = rightDropedNumbers.Length - 1, counter = leftDropedNumbers.Length; i >= 0; i--)
+        {
+            foldedNumbers[counter] = rightDropedNumbers[i];
+            counter++;
+        }
+
+        //Calculating the sum.
+
+        int[] sumArray = new int[reservedNumbers.Length];
+
+        for (int i = 0; i < sumArray.Length; i++)
+        {
+            sumArray[i] = foldedNumbers[i] + reservedNumbers[i];
+        }
+
+        ///Output
+        Console.WriteLine(string.Join(" ", sumArray));
+    }
+}
