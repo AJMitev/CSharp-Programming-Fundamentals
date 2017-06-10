4a2586548ed599a0a91f186d951f7b31c83ab1cf
ajmitev <ajmitev@gmail.com>
2017-06-10 09:26:01 +0300

    Update Problem 1

---
+++ 04.Arrays/Problem 01. Largest Common End/LargestCommonEnd.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+public class LargestCommonEnd
+{
+    public static void Main()
+    {
+        //Input
+        string[] firstInput = Console.ReadLine().Split(' ');
+        string[] secondInput = Console.ReadLine().Split(' ');
+
+        int maxLength = Math.Min(firstInput.Length, secondInput.Length);
+        int leftCntr = 0;
+        int rightCntr = 0;
+        int breaker = 0;
+        string[] commonPart = new string[maxLength];
+
+
+        //Left Side
+        for (int i = 0; i < maxLength; i++)
+        {
+            if (firstInput[i] == secondInput[i])
+            {
+                leftCntr++;
+                commonPart[i] = firstInput[i];
+            }
+        }
+
+        //Right Side
+        for (int i = maxLength; i > 0; i--)
+        {
+            if (firstInput[firstInput.Length - 1 - breaker] == secondInput[secondInput.Length - 1 - breaker])
+            {
+                rightCntr++;
+                breaker++;
+                commonPart[commonPart.Length - i] = firstInput[firstInput.Length - 1 - breaker];
+            }
+        }
+
+        if (leftCntr > 0)
+        {
+            Console.WriteLine(leftCntr);
+        }
+        else if (rightCntr > 0)
+        {
+            Console.WriteLine(rightCntr);
+        }
+        else
+        {
+            Console.WriteLine("0");
+        }
+
+    }
+}
