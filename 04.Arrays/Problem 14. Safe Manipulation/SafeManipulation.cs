ca8fee0e926c87e3866eece445d02a27b8f0a246
Aleksandar <aleksanderj.mitev@students.softuni.bg>
2017-06-12 01:18:14 +0300

    Update Problems 13 & 14

---
+++ 04.Arrays/Problem 14. Safe Manipulation/SafeManipulation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+public class SafeManipulation
+{
+    public static void Main()
+    {
+        //Input
+        string[] arrayOfWords = Console.ReadLine().Split(' ');
+
+        //Logic
+        while (true)
+        {
+            string[] command = Console.ReadLine().Split(' ');
+
+            bool isEnded = false;
+
+            switch (command[0])
+            {
+                case "END": isEnded = true; break;
+                case "Reverse": Array.Reverse(arrayOfWords); break;
+                case "Distinct": arrayOfWords = arrayOfWords.Distinct().ToArray(); break;
+                case "Replace":
+                    if (int.Parse(command[1]) > arrayOfWords.Length - 1 ||  int.Parse(command[1]) < 0)
+                    {
+                        Console.WriteLine("Invalid input!");
+                        break;
+                    }
+                    else
+                    {
+                        arrayOfWords[int.Parse(command[1])] = command[2];
+                    }
+                    break;
+                default: Console.WriteLine("Invalid input!"); break;
+            }
+
+            if (isEnded)
+            {
+                break;
+            }
+        }
+
+        //Output
+        Console.WriteLine(string.Join(", ", arrayOfWords));
+    }
+}
