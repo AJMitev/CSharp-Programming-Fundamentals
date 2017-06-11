977fc999ad7643ada68a43c73c310398911efa3e
Aleksandar <aleksanderj.mitev@students.softuni.bg>
2017-06-12 01:17:51 +0300

    Update Problems 13 & 14

---
+++ 04.Arrays/Problem 13. Manipulate Array/ManipulateArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+public class ManipulateArray
+{
+    public static void Main()
+    {
+        //Input
+        string[] arrayOfWords = Console.ReadLine().Split(' ');
+        int manipulationsCount = int.Parse(Console.ReadLine());
+
+        //Logic
+        for (int i = 0; i < manipulationsCount; i++)
+        {
+            string[] command = Console.ReadLine().Split(' ');
+
+            switch (command[0])
+            {
+                case "Reverse":  Array.Reverse(arrayOfWords);  break;
+                case "Distinct": arrayOfWords = arrayOfWords.Distinct().ToArray(); break;
+                case "Replace": arrayOfWords[int.Parse(command[1])] = command[2]; break;
+            }
+         
+        }
+
+        Console.WriteLine(string.Join(", ", arrayOfWords));
+    }
+}
