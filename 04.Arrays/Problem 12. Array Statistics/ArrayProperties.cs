46bf174899eea6e919c80492cc803a17593b3681
Aleksandar <aleksanderj.mitev@students.softuni.bg>
2017-06-11 23:36:05 +0300

    Update Problem 12

---
+++ 04.Arrays/Problem 12. Array Statistics/ArrayProperties.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+public class ArrayProperties
+{
+    public static void Main()
+    {
+        //Input
+        int[] arrayOfNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        //Logic
+        int max = arrayOfNumbers[0];
+        int min = arrayOfNumbers[0];
+        int sum = arrayOfNumbers[0];
+
+        for (int i = 1; i < arrayOfNumbers.Length; i++)
+        {
+            max = Math.Max(max, arrayOfNumbers[i]);
+            min = Math.Min(min, arrayOfNumbers[i]);
+            sum += arrayOfNumbers[i];
+        }
+
+        double average = (double)sum / arrayOfNumbers.Length;
+
+
+    //Output
+    Console.WriteLine($"Min = {min}\nMax = {max}\nSum = {sum}\nAverage = {average}");
+    }
+}
