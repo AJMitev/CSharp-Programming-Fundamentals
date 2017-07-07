ab49a1341e65c1c8a70dd32166f82ca3a725a19c
ajmitev <ajmitev@gmail.com>
2017-07-07 23:35:53 +0300

    Update after tenth lecture.

---
+++ 09.Strings and Text Processing/Problem 13. Email Statistics/EmailStatistics.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class EmailStatistics
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string emailValidation = @"^(?<name>[a-zA-Z]{5,})@(?<domain>[a-z]{3,}\.(?:com|bg|org))$";
+
+        var emailsList = new Dictionary<string, List<string>> ();
+
+        for (int i = 0; i < n; i++)
+        {
+            string line = Console.ReadLine();
+            var validEmail = Regex.Match(line, emailValidation);
+            if (validEmail.Success)
+            {
+                string username = validEmail.Groups["name"].Value;
+                string domain = validEmail.Groups["domain"].Value;
+
+                if (!emailsList.ContainsKey(domain))
+                {
+                    emailsList.Add(domain, new List<string>());
+                }
+
+                if (!emailsList[domain].Contains(username))
+                {
+                    emailsList[domain].Add(username);
+                }
+            }
+        }
+
+        emailsList = emailsList
+            .Distinct()
+            .OrderByDescending(u => u.Value.Count)
+             .ToDictionary(k => k.Key, v => v.Value);
+
+        foreach (var domain in emailsList)
+        {
+            Console.WriteLine($"{domain.Key}:");
+            foreach (var username in domain.Value)
+            {
+                Console.WriteLine($"### {username}");
+            }
+        }
+    }
+}
---
+++ 09.Strings and Text Processing/Problem 14. Hideout/Hideout.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Hideout
+{
+    public static void Main()
+    {
+        string path = Console.ReadLine();
+
+        while (true)
+        {
+            string[] tokens = Console.ReadLine().Split();
+
+            string pattern = $@"\{tokens[0]}{{{tokens[1]},}}";
+
+            Match match = Regex.Match(path, pattern);
+            if (match.Success)
+            {
+                Console.WriteLine($"Hideout found at index {path.IndexOf(match.Value)} and it is with size {match.Value.Length}!");
+                break;
+            }
+
+        }
+
+    }
+}
