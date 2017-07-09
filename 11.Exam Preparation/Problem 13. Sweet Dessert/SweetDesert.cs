43597fa65cad09ccc7d10e16b312c9383a9ade61
ajmitev <ajmitev@gmail.com>
2017-07-09 15:01:46 +0300

    Update after fourth preparation

---
+++ 11.Exam Preparation/Problem 13. Sweet Dessert/SweetDesert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SweetDesert
+{
+    public static void Main()
+    {
+        //Input
+        double money = double.Parse(Console.ReadLine());
+        int numberOfGuests = int.Parse(Console.ReadLine());
+        double bananasPrice = double.Parse(Console.ReadLine());
+        double eggsPrice = double.Parse(Console.ReadLine());
+        double berriesPrice = double.Parse(Console.ReadLine());
+
+        //Logic
+        double numberOfSets = Math.Ceiling(numberOfGuests / 6.0);
+        double moneyNeeded = ((bananasPrice * 2) + (eggsPrice * 4) + (berriesPrice * 0.2)) * numberOfSets;
+
+        //Output
+        if (money >= moneyNeeded)
+        {
+            Console.WriteLine($"Ivancho has enough money - it would cost {moneyNeeded:f2}lv.");
+        }
+        else
+        {
+            Console.WriteLine("Ivancho will have to withdraw money - he will need {0:f2}lv more.", Math.Abs(money-moneyNeeded));
+        }
+
+    }
+}
---
+++ 11.Exam Preparation/Problem 14. Array Manipulator/ArrayManipulator.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class ArrayManipulator
+{
+    public static void Main()
+    {
+        var numberArray = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+        string line = Console.ReadLine();
+
+        while (line != "end")
+        {
+
+            var command = line.Split();
+
+            string action = command[0];
+            switch (action)
+            {
+                case "exchange":
+                    int index = int.Parse(command[1]);
+                    index = (index % numberArray.Count) + 1;
+
+                    if (index >= 0 && index <= numberArray.Count)
+                    {
+                        ArrayExchange(numberArray, index);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                    break;
+                case "max":
+                    string maxType = command[1];
+                    int maxIndex = GetMaxIndex(numberArray, maxType);
+                    if (maxIndex.Equals(-1))
+                    {
+                        Console.WriteLine("No matches");
+                    }
+                    break;
+                case "min":
+                    string minType = command[1];
+                    int minIndex = GetMinIndex(numberArray, minType);
+                    if (minIndex.Equals(-1))
+                    {
+                        Console.WriteLine("No matches");
+                    }
+                    break;
+                case "first":
+                    FindFirst(numberArray, command);
+                    break;
+                case "last":
+                    break;
+                default:
+                    break;
+            }
+
+
+            line = Console.ReadLine();
+        }
+    }
+
+    private static void FindFirst(List<int> numberArray, string[] command)
+    {
+        int countFirst = int.Parse(command[1]);
+        int[] firsOfType = new int[countFirst];
+
+        for (int i = 0; i < countFirst; i++)
+        {
+
+        }
+        var currentNum = numberArray.FindIndex(0,countFirst,x => x % 2 == 2);
+    }
+
+    private static int GetMinIndex(List<int> numberArray, string type)
+    {
+        int currentOperator = 0;
+        if (type.Equals("even"))
+        {
+            currentOperator = 2;
+        }
+        else if (type.Equals("odd"))
+        {
+            currentOperator = 1;
+        }
+
+        int minIndex = -1;
+        int minNumber = 0;
+
+        for (int i = 0; i < numberArray.Count; i++)
+        {
+            int currentNumber = numberArray[i];
+
+            if (i % 2 == currentOperator && minNumber < currentNumber)
+            {
+                minIndex = i;
+            }
+        }
+
+        return minIndex;
+    }
+    private static int GetMaxIndex(List<int> numberArray, string type)
+    {
+        int currentOperator = 0;
+        if (type.Equals("even"))
+        {
+            currentOperator = 2;
+        }
+        else if (type.Equals("odd"))
+        {
+            currentOperator = 1;
+        }
+
+        int maxIndex = -1;
+        int maxNumber = 0;
+
+        for (int i = 0; i < numberArray.Count; i++)
+        {
+            int currentNumber = numberArray[i];
+
+            if (i % 2 == currentOperator && maxNumber > currentNumber)
+            {
+                maxIndex = i;
+            }
+        }
+
+        return maxIndex;
+    }
+
+    private static void ArrayExchange(List<int> numberArray, int index)
+    {
+
+        for (int i = 0; i < index; i++)
+        {
+            numberArray.Insert(numberArray.Count, numberArray[0]);
+            numberArray.Remove(numberArray[0]);
+        }
+    }
+}
